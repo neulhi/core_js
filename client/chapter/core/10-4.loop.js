66255f41f09140c5e4a72a1cc75f17ea9b431f7e
neulhi <neu1hi.work@gmail.com>
2024-11-05 17:02:08 +0900

    learn. core javascript
    - loop (for..in)
    - loop (for..of)
    - loop (for..of)
    - function (함수 선언문)

--- client/chapter/core/10-4.loop.js
+++ client/chapter/core/10-4.loop.js
@@ -14,9 +14,10 @@ const javaScript = {
   }
 };
 
+// 조상(Object)에 접근해서 능력에 추가 전역 오염
 Object.prototype.nickName = 'tiger'
 
-
+// 조상(Object) 까지 찾아봐서 있다고 판단
 'toString' in javaScript  // true
 
 
@@ -31,14 +32,104 @@ Object.prototype.nickName = 'tiger'
 
 // console.log( javaScript.hasOwnProperty('toString') );
 
-console.log( Object.prototype.hasOwnProperty.call(javaScript,'creator') );
+// console.log( Object.prototype.hasOwnProperty.call(javaScript,'creator') );
 
-console.log( Object.hasOwn(javaScript,'nickName') );
-
-
+// console.log( Object.hasOwn(javaScript,'nickName') );
 
 
+// const obj = { }
 
-// for ~ in 문
+// const hello = 'a'
+
+// hello.toLowerCase() // 'a'
+// obj.toLowerCase() // 에러
+
+// in 문은 하나 조회
+// fon...in 문은 여러개 조회
+
+// for...in 문
 // - 객체 자신의 속성만 순환하려면?
+
+// 객체의 key를 조회 
+// 객체의 value를 조회 
+
+for( const key in javaScript ){
+
+  // if(Object.hasOwn(javaScript,key))
+  if(Object.prototype.hasOwnProperty.call(javaScript,key)){
+
+    const value = javaScript[key];
+    
+    console.log( value );
+    
+  }
+  
+}
+
+console.clear();
+
+
+
 // - 배열 객체 순환에 사용할 경우?
+// 배열도 for...in문으로 순환이 가능은 하다.
+// for...in문은 객체에 사용 권장
+
+// 배열은 순서에 굉장히 민감한 데이터 => for ~ in은 순서를 보장하지 않는다.
+
+const tens = [10,100,1000,10_000];
+
+for (const key in tens) {
+	if (Object.prototype.hasOwnProperty.call(tens, key)) {
+		const value = tens[key];
+
+		// console.log(value);
+		
+	}
+}
+
+// enumerable : 열거 가능한 / for문으로 조회 가능한
+
+for( const key in javaScript){
+  console.log(key);
+  
+}
+
+
+
+/* ----------------------------------- 참고만 ---------------------------------- */
+
+const obj = {}
+
+
+
+obj.nickName = 'tiger'
+
+
+Object.defineProperty(obj,'age',{
+  value:30,
+  enumerable:true,
+  writable:false,
+  configurable:false
+})
+
+Object.defineProperties(obj,{
+  age:{
+    value:30,
+    enumerable:true,
+    writable:false,
+    configurable:false
+  },
+  email:{
+    
+  }
+})
+
+
+
+console.log(obj);
+
+
+for( const key in obj){
+  console.log(key);
+  
+}
