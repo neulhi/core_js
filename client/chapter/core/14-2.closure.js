f3780133a9a22f40ddcabb74e785ed1945fa9d80
neulhi <neu1hi.work@gmail.com>
2024-11-12 12:57:55 +0900

    learn. core javascript - closure

---
+++ client/chapter/core/14-2.closure.js
@@ -0,0 +1,28 @@
+function earth() {
+  let water = true;
+  let gravity = 10;
+
+  return (value) => [water, gravity];
+}
+
+const ufo = earth();
+
+ufo(false);
+
+// 개념은 이해 했습니다. 근데, 어디다 써요?
+
+const button = document.querySelector('button');
+const handleClick = (() => {
+  let clicked = false;
+
+  return () => {
+    if (!clicked) {
+      document.body.style.background = 'orange';
+    } else {
+      document.body.style.background = 'white';
+    }
+
+    clicked = !clicked;
+  };
+})();
+button.addEventListener('click', handleClick);
