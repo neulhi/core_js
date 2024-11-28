3372e3af8e49afe692a0341945f770e598502fac
neulhi <neu1hi.work@gmail.com>
2024-11-28 11:57:41 +0900

    learn. todoList - todoListe - todoItem - 데이터 모델 분리 - service - gsap

--- client/main.js
+++ client/main.js
@@ -1,17 +1,30 @@
-import { Button } from './components/Button/Button.js';
-import { Card } from './components/Card/Card.js';
-import { Counter } from './components/Counter/Counter.js';
+
+
+
+import { Button } from "./components/Button/Button.js";
+import { Card } from "./components/Card/Card.js";
+import { Counter } from "./components/Counter/Counter.js";
+import { TodoList } from "./components/TodoList/TodoList.js";
+import { TodoItem } from "./components/TodoItem/TodoItem.js";
+
+
+
 
 const app = document.getElementById('app');
 
-function defineElements() {
-  customElements.define('custom-button', Button);
-  customElements.define('custom-card', Card);
-  customElements.define('custom-counter', Counter);
+
+
+function defineElements(){
+  customElements.define('custom-button',Button)
+  customElements.define('custom-card',Card)
+  customElements.define('custom-counter',Counter)
+  customElements.define('custom-todo',TodoList)
+  customElements.define('custom-todo-item',TodoItem)
 }
 
-defineElements();
+defineElements()
 
-const buttonElement = document.createElement('custom-button');
 
-// app.append(buttonElement);
+const todoElement = document.createElement('custom-todo');
+
+app.append(todoElement);
