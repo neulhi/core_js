ec65373d2f9887b485a2cba760574ed089c17d01
neulhi <neu1hi.work@gmail.com>
2024-11-29 10:26:02 +0900

    learn. vite - tainwildcss - webComponent - webComponent 안에서 style 불러오기

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-import '@/style/style.css';
-import { getNode, insertLast } from 'kind-tiger';
-import santa from '@/assets/santa.png'; // 하나만 가져오는거라 이름 변경 가능
-import { btn } from '@/style/style.module.css';
-
-const app = getNode('#app');
-
-const template = /* html */ `
-	<figure class="container">
-	<img style="width: 30vw" src="${santa}" />
-	<figcaption>산타 모자를 쓴 호랑이</figcaption>
-	</figure>
-	<button type="button" class="${btn}">BUTTON</button>
-`;
-
-insertLast(app, template);
+import {  } from '/src/Layout/Header/Header';
