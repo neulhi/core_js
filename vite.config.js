90fa5572c1e10d269214878298ca22ab15464f07
neulhi <neu1hi.work@gmail.com>
2024-12-05 10:57:04 +0900

    learn. pocketbase - login - pocketbase  SDK - sweetAlert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
-export default defineConfig ({
-	resolve: {
-		alias: [
-			{find:'@', replacement:'/src'}
-		]
-	}
+
+ const config = defineConfig({
+  resolve:{
+    alias:[
+      {find:"@", replacement:"/src"}
+    ]
+  },
+  build:{
+    outDir:'docs',
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname,'index.html'),
+        product: resolve(__dirname,'src/pages/product/index.html'),
+        login: resolve(__dirname,'src/pages/login/index.html'),
+      }
+    }
+  }
 })
+
+export default config
